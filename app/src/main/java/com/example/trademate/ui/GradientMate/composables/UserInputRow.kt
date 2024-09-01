f81702702f6a5aa945ed0c964469c56fbb88ea78
Thomas <Thomas.watson555@Gmail.com>
2024-09-02 06:58:21 +1200

    Design changes, Gradle update,

--- app/src/main/java/com/example/trademate/ui/GradientMate/composables/UserInputRow.kt
+++ app/src/main/java/com/example/trademate/ui/GradientMate/composables/UserInputRow.kt
@@ -3,7 +3,6 @@ package com.example.trademate.ui.composables
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.stringResource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.background
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -12,7 +11,6 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.trademate.R
